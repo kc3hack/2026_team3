1a34fc5fc8aeec7a081aeb560ae58805f6f20f58
Kyarasu <Setsuru.lilac@gmail.com>
2026-02-16 15:29:26 +0900

    Feature: Done

--- Backend/Workspace/Server.js
+++ Backend/Workspace/Server.js
@@ -1,26 +1,40 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+// 絶対パス指定
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const cookieParser = require('cookie-parser');
+// Routing自動化
+const fs = require('fs');
+const routesDir = path.join(__dirname, 'Routes');
+
 const app = express();
 
-// ポート番号
-const PORT = 3000;
+// ========== 初期設定 ==========
+PORT = process.env.PORT || 5000;
 
-// JSONを受け取れるようにする
-app.use(express.json());
+// ========== use系 ==========
+app.use(express.json);
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '..', '..', 'Frontend', 'dist')));
 
-// ルート
-app.get("/", (req, res) => {
-  res.send("Server is running!");
+// ========== Routes ==========
+// fs.readdirSync(ディレクトリパス)でそのディレクトリ内のファイル名を配列で取得
+fs.readdirSync(routesDir).forEach((file) => {
+    // js ファイルだけ対象
+    if (!file.endsWith('.js')) return;
+
+    // APIのエンドポイントをファイル名から作成
+    const routeName = path.basename(file, '.js');
+    const routePath = `/${routeName}`;
+
+    // Routing処理
+    const route = require(path.join(routesDir, file));
+    app.use(routePath, route);
+
+    // コンソール表示
+    console.log(`Route mounted: ${routePath}`);
 });
 
-app.post('/scan', (req, res) => {
-    const cardUid = req.body.uid; // Pythonから送られてきたUID
-    console.log(`カードを検知しました！ UID: ${cardUid}`);
-
-    // Python側に「無事に受け取ったよ」と返事をする
-    res.status(200).send({ message: 'Success' });
-});
-
-// サーバ起動
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// ========== listen ==========
+app.listen(PORT, '0.0.0.0', () => { console.log(`Server running at https://localhost:${PORT}`)} );
