15d13f8c7135ea63b1f1655489ded8baa740553b
nayu <is0762ix@ed.ritsumei.ac.jp>
2026-02-17 00:48:51 +0900

    Login, Register画面
    ほとんど同じなどでまとめて出してます

--- Frontend/src/Pages/Login/Login.tsx
+++ Frontend/src/Pages/Login/Login.tsx
@@ -1,11 +1,89 @@
 import "./Login.css";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import InputField from "../../Components/InputField/InputField";
+import ConfirmButton from "../../Components/ConfirmButton/ConfirmButton";
+import icon from "../../Components/Elements/icon.png";
 
 function Login() {
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    async function HandleLogin() {
+        try {
+            const res = await fetch('Login/Submit', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                body: JSON.stringify({ userId, password }),
+            });
+
+            if (!res.ok) {
+                const data = await res.json();
+                toast.error("ログインに失敗しました");
+                console.log("Faild: Login", data);
+                return;
+            } else {
+                toast.success("ログインしました");
+                console.log("Success: Login");
+                navigate("/Home");
+            }
+        } catch (err) {
+            toast.error('通信エラーが発生しました');
+            console.log("Faild: Communication");
+        }
+    }
+
     return (
-    <div>
-      Page
-    </div>
-  );
+        <form action="" method="POST">
+            <div className="LoginBackground">
+                <div className="LoginTab">
+                    <div className="LoginLeft">
+                        <img src={icon} className="LoginIcon" alt="" />
+                        <h1>ログイン</h1>
+                    </div>
+                </div>
+                <div className="LoginBox">
+                    <div className="LoginRegister">
+                        <div className="LoginInput">
+                            <div className="LoginOneset">
+                                <p>UserID</p>
+                                <InputField name="UserID" type="text"
+                                    onChange={(e) => {
+                                        setUserId(e.target.value);
+                                        setError("");
+                                    }}
+                                    placeholder="" />
+                                {error && <p className="LoginError">{error}</p>}
+                            </div>
+                            <div className="LoginOneset">
+                                <p>Password</p>
+                                <InputField name="Password" type="password"
+                                    onChange={(e) => {
+                                        setPassword(e.target.value);
+                                        setError("");
+                                    }}
+                                    placeholder="" />
+                                {error && <p className="LoginError">{error}</p>}
+                            </div>
+                        </div>
+                        <ConfirmButton label="送信"
+                            onClick={() => {
+                                if (userId === "" || password === "") {
+                                    setError("必要事項を入力してください");
+                                } else {
+                                    HandleLogin();
+                                }
+                            }}
+                            type="button" />
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default Login;
